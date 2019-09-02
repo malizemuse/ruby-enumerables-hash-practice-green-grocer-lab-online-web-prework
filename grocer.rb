dd139cd001fe53e2a0e0345d4b6a99ae40ebb576
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-02 03:48:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,6 +48,7 @@ def apply_coupons(cart, coupons)
       
       puts "This should print"
       
+      =begin
       # Subtract item count in cart by num of coupons 
       new_cart[c_name][:count] -= c_num
        
@@ -68,6 +69,7 @@ def apply_coupons(cart, coupons)
         puts "This should NOT print"
         
       =end
+      end
       
       end
       
